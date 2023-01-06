39cf3e7f6b82b4f8dd9a92229f424a3d7e2e0e06
Newton <altontonnalumasa@gmail.com>
2023-01-07 01:56:52 +0300

    remediate detected secret

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TOKEN = 'ces9kdiad3i2r4r9rgagces9kdiad3i2r4r9rgb0';
-export default axios.create({
+const TOKEN = ''
+;export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
     token: TOKEN
