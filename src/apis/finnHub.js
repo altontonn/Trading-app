09d283625ccff2ac8881569f799391064dc3c776
Newton <altontonnalumasa@gmail.com>
2023-01-07 18:11:28 +0300

    rename api key variable

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const TOKEN = 'cesog42ad3i1epsgfeugcesog42ad3i1epsgfev0';
-;export default axios.create({
+const key = 'cesog42ad3i1epsgfeugcesog42ad3i1epsgfev0';
+export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
-    token: TOKEN
+    token: key
   }
 })
