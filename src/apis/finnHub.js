cc1fc297e821a400c0971b36537f2c033222e521
Newton <altontonnalumasa@gmail.com>
2023-01-07 17:58:28 +0300

    add new api key

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TOKEN = ''
+const TOKEN = 'cesog42ad3i1epsgfeugcesog42ad3i1epsgfev0';
 ;export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
