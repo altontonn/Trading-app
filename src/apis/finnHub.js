3f5a710d0a4bcca40d766b03fbd77061817199b9
Newton <altontonnalumasa@gmail.com>
2023-01-07 23:05:47 +0300

    remove secret key

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const key = 'cesog42ad3i1epsgfeugcesog42ad3i1epsgfev0';
+const key = '';
 export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
