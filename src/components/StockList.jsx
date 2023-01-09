b52cd840957cf39f44116da78f5f417e8011c76b
Newton <altontonnalumasa@gmail.com>
2023-01-09 22:31:36 +0300

    use promise to get all data at once

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -2,30 +2,30 @@ import { useState, useEffect } from "react";
 import finnHub from "../apis/finnHub";
 
 export const StockList = () => {
-  let isMounted = true;
   // eslint-disable-next-line
   const [stock, setStock] = useState();
+  // eslint-disable-next-line
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
   useEffect(() => {
+    let isMounted = true;
     const fetchData = async () => {
+      //const responses = [];
       try {
-        const response = await finnHub.get('/quote', {
-          params: {
-            symbol: 'MSFT'
-          }
-        });
-        console.log(response);
+        const responses = await Promise.all(watchList.map((stock) => {
+          return finnHub.get("/quote", {
+            params: {
+              symbol: stock
+            }
+          })
+        }));
+        console.log(responses);
         if (isMounted) {
-          setStock(response.data)
+          setStock(responses);
         }
-      } catch (error) {
-
-      }
-    }
+      } catch (error) {}
+    };
     fetchData();
     return () => (isMounted = false);
-  }, [])
-  return (
-    <div>StockList</div>
-  )
+  }, []);
+  return <div>StockList</div>;
 };
