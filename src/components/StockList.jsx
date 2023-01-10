e980a3aceb28336bb9c0f1a6764dd4e60b1914e6
Newton <altontonnalumasa@gmail.com>
2023-01-10 23:11:09 +0300

    add a table

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -34,5 +34,40 @@ export const StockList = () => {
     fetchData();
     return () => (isMounted = false);
   }, []);
-  return <div>StockList</div>;
+  return (
+    <div>
+      <table className="table hover mt-5">
+        <thead style={{color: "rgb(79, 89, 102)"}}>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Last</th>
+            <th scope="col">Chg</th>
+            <th scope="col">Chg%</th>
+            <th scope="col">High</th>
+            <th scope="col">Low</th>
+            <th scope="col">Open</th>
+            <th scope="col">Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            stock.map((stockData) => {
+              return (
+                <tr className="table-row" key={stockData.symbol}>
+                  <th scope="row">{stockData.symbol}</th>
+                  <td>{stockData.data.c}</td>
+                  <td>{stockData.data.d}</td>
+                  <td>{stockData.data.dp}</td>
+                  <td>{stockData.data.h}</td>
+                  <td>{stockData.data.l}</td>
+                  <td>{stockData.data.o}</td>
+                  <td>{stockData.data.pc}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
 };
