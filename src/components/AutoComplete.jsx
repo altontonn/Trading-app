aa56b7c64ce1daf91dd9acc18b210b09c20d2da4
Newton <altontonnalumasa@gmail.com>
2023-01-12 00:51:22 +0300

    add an autocomplete search component

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,4 +1,68 @@
+import { useState, useEffect } from "react";
+import finnHub from "../apis/finnHub";
+
 export const AutoComplete = () => {
-    return <div>AutoComplete</div>;
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const renderDropDown = () => {
+    const dropDownClass = search ? "show" : null;
+    return (
+      <ul
+        style={{
+          height: "500px",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          cursor: "pointer",
+        }}
+        className={`dropdown-menu ${dropDownClass}`}
+      >
+        {results.map((result) => {
+          return (
+            <li key={result.symbol} className="dropdown-item">
+              {result.description} {result.symbol}
+            </li>
+          );
+        })}
+      </ul>
+    );
   };
-  
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/search", {
+          params: {
+            q: search,
+          },
+        });
+        if (isMounted) {
+          setResults(response.data.result);
+        }
+      } catch (err) {}
+    };
+    if (search.length > 0) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+    return () => (isMounted = false);
+  }, [search]);
+  return (
+    <div className="w-50 p-5 rounded mx-auto">
+      <div className="form-floating dropdown">
+        <input
+          style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
+          id="search"
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          autoComplete="off"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <label htmlFor="search">Search</label>
+        {renderDropDown()}
+      </div>
+    </div>
+  );
+};
